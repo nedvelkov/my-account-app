b4a730678825cea0af4981efef597acc64b5eeb5
Nedyalko Velkov <nedyalkovelkov@gmail.com>
2025-11-01 12:16:18 +0200

    add auth0 provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId= import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: window.location.href }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
